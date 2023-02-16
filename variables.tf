8ea1662d85e2135bea364e20e894c26432486fc1
Xiao Zhang <zhang.xiao12@northeastern.edu>
2023-02-15 22:53:53 -0500

    Update variable default

--- variables.tf
+++ variables.tf
@@ -1,19 +1,15 @@
 variable "vpc_region" {
-  type    = string
-  default = "us-east-1"
+  type = string
 }
 
 variable "name" {
-  type    = string
-  default = "network1"
+  type = string
 }
 
 variable "profile" {
   type    = string
-  default = "dev"
 }
 
 variable "cidr_block" {
-  type    = string
-  default = "10.0.0.0/16"
+  type = string
 }
